5ef60df3ec78d4f0ea2ed9825055746c76592e61
harD1kkkk <137618635+harD1kkkk@users.noreply.github.com>
2025-05-12 11:54:17 +0300

    Update lab_12.1.cpp

--- LAB_12.1/LAB_12.1/lab_12.1.cpp
+++ LAB_12.1/LAB_12.1/lab_12.1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 struct Elem {
@@ -8,74 +8,31 @@ struct Elem {
 };
 
 int main() {
-    Elem* p = new Elem;      
-    p->info = 1;
+    Elem* p = new Elem; // 1
+    p->info = 1;  
 
-    p->link2 = new Elem;
-    p->link2->info = 2;
+    p->link2 = new Elem; // 2
+    p->link2->info = 2; // 3
 
-    p->link1 = new Elem;
-    p->link1->info = 4;
+    p->link1 = new Elem; // 4
+    p->link1->info = 4; // 5
 
-    p->link2->link1 = new Elem;
-    p->link2->link1->info = 3;
+    p->link2->link1 = new Elem; // 6
+    p->link2->link1->info = 3; // 7
 
-    p->link2->link2 = nullptr;         
-    p->link2->link1->link2 = nullptr;           
+    p->link2->link2 = nullptr; // 8
+    p->link2->link1->link2 = nullptr; // 9
 
-    p->link2->link1->link1 = p->link2;         
-    p->link1->link1 = p->link2;          
-    p->link1->link2 = p->link2->link1;  
+    p->link2->link1->link1 = p->link2; // 10
+    p->link1->link1 = p->link2; // 11
+    p->link1->link2 = p->link2->link1; // 12
 
-    Elem* n2 = p->link1;
-    Elem* n3 = n2->link1;
-    Elem* n4 = p->link2;
 
-    delete n4;    
-    delete n3;
-    delete n2;
+
+    delete p->link2->link1;
+    delete p->link1;
+    delete p->link2;
     delete p;
 
     return 0;
 }
-/*#include <iostream>
-using namespace std;
-
-struct Elem {
-    Elem* link1;
-    Elem* link2;
-    int info;
-};
-
-int main() {
-    Elem* p = new Elem;      
-    p->info = 1;
-
-    p->link1 = new Elem;
-    p->link1->info = 2;
-
-    p->link2 = new Elem;
-    p->link2->info = 4;
-
-    p->link1->link1 = new Elem;
-    p->link1->link1->info = 3;
-
-    p->link1->link2 = nullptr;         
-    p->link1->link1->link2 = nullptr;           
-
-    p->link1->link1->link1 = p->link1;         
-    p->link2->link1 = p->link1;          
-    p->link2->link2 = p->link1->link1;  
-
-    Elem* n2 = p->link1;
-    Elem* n3 = n2->link1;
-    Elem* n4 = p->link2;
-
-    delete n4;    
-    delete n3;
-    delete n2;
-    delete p;
-
-    return 0;
-}
-*/
